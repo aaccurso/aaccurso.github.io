c669298611b6f4242ce166ab386562764bd25a40
aaccurso <accurso.alan@gmail.com>
2016-07-13 22:52:32 -0300

    Gets list of github repos

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 (function () {
-  console.log('Loading main.js');
+  const github_api = 'https://api.github.com';
+  const headers = new Headers({
+    'Authorization': 'token dcf74b88a1513ec5220e388b51d761055fb2c63e',
+    'Accept': 'application/vnd.github.v3+json'
+  });
+
+  fetch(github_api + '/user/repos', {headers})
+    .then(function (result) {
+      return result.json();
+    })
+    .then(function (json) {
+      console.log(json);
+    });
 })();
