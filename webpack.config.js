afede2b0f43e178d33b461f2c47b7322b717273a
aaccurso <accurso.alan@gmail.com>
2016-07-19 21:28:26 -0300

    Adds .babelrc and changes indentation to spaces

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,15 @@
 module.exports = {
-	entry: './_src/app.js',
-	output: {
-		path: __dirname + "/js",
-		filename: "main.js"
-	},
-	module: {
-	  loaders: [
-		{
-		  test: /\.js$/,
-		  loader: 'babel',
-		  query: {
-			presets: ['es2015']
-		  }
-		}
-	  ]
-	}
+  entry: './_src/app.js',
+  output: {
+    path: __dirname + "/js",
+    filename: "main.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel'
+      }
+    ]
+  }
 };
