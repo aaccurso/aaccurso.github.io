bbb8c3c3e853aafc318cf0d32a6abb073946f246
aaccurso <accurso.alan@gmail.com>
2017-02-22 00:44:54 -0300

    Add emoji parser for repo description

--- _src/components/github-repo-card.js
+++ _src/components/github-repo-card.js
@@ -1,19 +1,35 @@
 import { component } from 'rockyjs';
+import { load as loadEmoji, parse as parseEmoji } from 'gh-emoji'
 
 component.register('github-repo-card', {
   init (Github) {
     const name = this.attributes.getNamedItem('name').value;
 
-    return Github.repo(name)
-      .then(repo => {
-        return {repo};
+    return Promise.all([
+        Github.repo(name),
+        loadEmoji()
+      ])
+      .then(([repo]) => {
+        repo.description = parseEmoji(repo.description);
+        return {
+          repo,
+          owner: repo.owner
+        };
       });
   },
   template: `
     <div>
-      {{repo.name}}
-      Stars: {{repo.stargazers_count}}
-      Forks: {{repo.forks_count}}
+      <p>
+        {{repo.name}}
+        Stars: {{repo.stargazers_count}}
+        Forks: {{repo.forks_count}}
+        Language: {{repo.language}}
+        Description: {{{repo.description}}}
+        Owner:
+        - {{owner.login}}
+        - {{owner.html_url}}
+        <img src="{{owner.avatar_url}}" alt="{{owner.login}}"/>
+      </p>
     </div>
   `
 });
--- js/main.js
+++ js/main.js
@@ -44,17 +44,23 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;(function (global, factory) {
+	  if (true) {
+	    !(__WEBPACK_AMD_DEFINE_ARRAY__ = [__webpack_require__(1), __webpack_require__(4), __webpack_require__(5), __webpack_require__(6)], __WEBPACK_AMD_DEFINE_FACTORY__ = (factory), __WEBPACK_AMD_DEFINE_RESULT__ = (typeof __WEBPACK_AMD_DEFINE_FACTORY__ === 'function' ? (__WEBPACK_AMD_DEFINE_FACTORY__.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__)) : __WEBPACK_AMD_DEFINE_FACTORY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+	  } else if (typeof exports !== "undefined") {
+	    factory(require('rockyjs'), require('./services/github'), require('./components/github-repo-list'), require('./components/github-repo-card'));
+	  } else {
+	    var mod = {
+	      exports: {}
+	    };
+	    factory(global.rockyjs, global.github, global.githubRepoList, global.githubRepoCard);
+	    global.app = mod.exports;
+	  }
+	})(this, function (_rockyjs) {
+	  'use strict';
 
-	var _rockyjs = __webpack_require__(1);
-
-	__webpack_require__(4);
-
-	__webpack_require__(5);
-
-	__webpack_require__(6);
-
-	(0, _rockyjs.bootstrap)();
+	  (0, _rockyjs.bootstrap)();
+	});
 
 /***/ },
 /* 1 */
@@ -200,70 +206,407 @@
 /* 4 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;(function (global, factory) {
+	  if (true) {
+	    !(__WEBPACK_AMD_DEFINE_ARRAY__ = [__webpack_require__(1)], __WEBPACK_AMD_DEFINE_FACTORY__ = (factory), __WEBPACK_AMD_DEFINE_RESULT__ = (typeof __WEBPACK_AMD_DEFINE_FACTORY__ === 'function' ? (__WEBPACK_AMD_DEFINE_FACTORY__.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__)) : __WEBPACK_AMD_DEFINE_FACTORY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+	  } else if (typeof exports !== "undefined") {
+	    factory(require('rockyjs'));
+	  } else {
+	    var mod = {
+	      exports: {}
+	    };
+	    factory(global.rockyjs);
+	    global.github = mod.exports;
+	  }
+	})(this, function (_rockyjs) {
+	  'use strict';
 
-	var _rockyjs = __webpack_require__(1);
+	  _rockyjs.service.register('Github', function () {
+	    var github = {
+	      api: 'https://api.github.com',
+	      user: 'aaccurso',
+	      token: 'dcf74b88a1513ec5220e388b51d761055fb2c63e'
+	    };
+	    var headers = new Headers({
+	      'Authorization': 'token ' + github.token,
+	      'Accept': 'application/vnd.github.v3+json'
+	    });
+	    var toJson = function toJson(result) {
+	      return result.json();
+	    };
 
-	_rockyjs.service.register('Github', function () {
-	  var github = {
-	    api: 'https://api.github.com',
-	    user: 'aaccurso',
-	    token: 'dcf74b88a1513ec5220e388b51d761055fb2c63e'
-	  };
-	  var headers = new Headers({
-	    'Authorization': 'token ' + github.token,
-	    'Accept': 'application/vnd.github.v3+json'
+	    return {
+	      repos: function repos() {
+	        return fetch(github.api + '/user/repos', { headers: headers }).then(toJson);
+	      },
+	      repo: function repo(name) {
+	        return fetch(github.api + '/repos/' + github.user + '/' + name, { headers: headers }).then(toJson);
+	      }
+	    };
 	  });
-	  var toJson = function toJson(result) {
-	    return result.json();
-	  };
-
-	  return {
-	    repos: function repos() {
-	      return fetch(github.api + '/user/repos', { headers: headers }).then(toJson);
-	    },
-	    repo: function repo(name) {
-	      return fetch(github.api + '/repos/' + github.user + '/' + name, { headers: headers }).then(toJson);
-	    }
-	  };
 	});
 
 /***/ },
 /* 5 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;(function (global, factory) {
+	  if (true) {
+	    !(__WEBPACK_AMD_DEFINE_ARRAY__ = [__webpack_require__(1)], __WEBPACK_AMD_DEFINE_FACTORY__ = (factory), __WEBPACK_AMD_DEFINE_RESULT__ = (typeof __WEBPACK_AMD_DEFINE_FACTORY__ === 'function' ? (__WEBPACK_AMD_DEFINE_FACTORY__.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__)) : __WEBPACK_AMD_DEFINE_FACTORY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+	  } else if (typeof exports !== "undefined") {
+	    factory(require('rockyjs'));
+	  } else {
+	    var mod = {
+	      exports: {}
+	    };
+	    factory(global.rockyjs);
+	    global.githubRepoList = mod.exports;
+	  }
+	})(this, function (_rockyjs) {
+	  'use strict';
 
-	var _rockyjs = __webpack_require__(1);
+	  _rockyjs.component.register('github-repo-list', {
+	    init: function init(Github) {
+	      return Github.repos().then(function (repos) {
+	        return { repos: repos };
+	      });
+	    },
 
-	_rockyjs.component.register('github-repo-list', {
-	  init: function init(Github) {
-	    return Github.repos().then(function (repos) {
-	      return { repos: repos };
-	    });
-	  },
-
-	  template: '\n    <ul>\n    {{#each repos}}\n      <li><a href="{{url}}">{{name}}</a></li>\n    {{/each}}\n    </ul>\n  '
+	    template: '\n    <ul>\n    {{#each repos}}\n      <li><a href="{{url}}">{{name}}</a></li>\n    {{/each}}\n    </ul>\n  '
+	  });
 	});
 
 /***/ },
 /* 6 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;(function (global, factory) {
+	  if (true) {
+	    !(__WEBPACK_AMD_DEFINE_ARRAY__ = [__webpack_require__(1), __webpack_require__(7)], __WEBPACK_AMD_DEFINE_FACTORY__ = (factory), __WEBPACK_AMD_DEFINE_RESULT__ = (typeof __WEBPACK_AMD_DEFINE_FACTORY__ === 'function' ? (__WEBPACK_AMD_DEFINE_FACTORY__.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__)) : __WEBPACK_AMD_DEFINE_FACTORY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+	  } else if (typeof exports !== "undefined") {
+	    factory(require('rockyjs'), require('gh-emoji'));
+	  } else {
+	    var mod = {
+	      exports: {}
+	    };
+	    factory(global.rockyjs, global.ghEmoji);
+	    global.githubRepoCard = mod.exports;
+	  }
+	})(this, function (_rockyjs, _ghEmoji) {
+	  'use strict';
 
-	var _rockyjs = __webpack_require__(1);
+	  var _slicedToArray = function () {
+	    function sliceIterator(arr, i) {
+	      var _arr = [];
+	      var _n = true;
+	      var _d = false;
+	      var _e = undefined;
 
-	_rockyjs.component.register('github-repo-card', {
-	  init: function init(Github) {
-	    var name = this.attributes.getNamedItem('name').value;
+	      try {
+	        for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) {
+	          _arr.push(_s.value);
 
-	    return Github.repo(name).then(function (repo) {
-	      return { repo: repo };
+	          if (i && _arr.length === i) break;
+	        }
+	      } catch (err) {
+	        _d = true;
+	        _e = err;
+	      } finally {
+	        try {
+	          if (!_n && _i["return"]) _i["return"]();
+	        } finally {
+	          if (_d) throw _e;
+	        }
+	      }
+
+	      return _arr;
+	    }
+
+	    return function (arr, i) {
+	      if (Array.isArray(arr)) {
+	        return arr;
+	      } else if (Symbol.iterator in Object(arr)) {
+	        return sliceIterator(arr, i);
+	      } else {
+	        throw new TypeError("Invalid attempt to destructure non-iterable instance");
+	      }
+	    };
+	  }();
+
+	  _rockyjs.component.register('github-repo-card', {
+	    init: function init(Github) {
+	      var name = this.attributes.getNamedItem('name').value;
+
+	      return Promise.all([Github.repo(name), (0, _ghEmoji.load)()]).then(function (_ref) {
+	        var _ref2 = _slicedToArray(_ref, 1);
+
+	        var repo = _ref2[0];
+
+	        repo.description = (0, _ghEmoji.parse)(repo.description);
+	        return {
+	          repo: repo,
+	          owner: repo.owner
+	        };
+	      });
+	    },
+
+	    template: '\n    <div>\n      <p>\n        {{repo.name}}\n        Stars: {{repo.stargazers_count}}\n        Forks: {{repo.forks_count}}\n        Language: {{repo.language}}\n        Description: {{{repo.description}}}\n        Owner:\n        - {{owner.login}}\n        - {{owner.html_url}}\n        <img src="{{owner.avatar_url}}" alt="{{owner.login}}"/>\n      </p>\n    </div>\n  '
+	  });
+	});
+
+/***/ },
+/* 7 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;(function (global, factory) {
+	  if (true) {
+	    !(__WEBPACK_AMD_DEFINE_ARRAY__ = [exports], __WEBPACK_AMD_DEFINE_FACTORY__ = (factory), __WEBPACK_AMD_DEFINE_RESULT__ = (typeof __WEBPACK_AMD_DEFINE_FACTORY__ === 'function' ? (__WEBPACK_AMD_DEFINE_FACTORY__.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__)) : __WEBPACK_AMD_DEFINE_FACTORY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+	  } else if (typeof exports !== "undefined") {
+	    factory(exports);
+	  } else {
+	    var mod = {
+	      exports: {}
+	    };
+	    factory(mod.exports);
+	    global.ghEmoji = mod.exports;
+	  }
+	})(this, function (exports) {
+	  "use strict";
+
+	  (function (global, factory) {
+	    if (true) {
+	      !(__WEBPACK_AMD_DEFINE_ARRAY__ = [], __WEBPACK_AMD_DEFINE_FACTORY__ = (factory), __WEBPACK_AMD_DEFINE_RESULT__ = (typeof __WEBPACK_AMD_DEFINE_FACTORY__ === 'function' ? (__WEBPACK_AMD_DEFINE_FACTORY__.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__)) : __WEBPACK_AMD_DEFINE_FACTORY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+	    } else if (typeof exports !== "undefined") {
+	      factory();
+	    } else {
+	      var mod = {
+	        exports: {}
+	      };
+	      factory();
+	      global.types = mod.exports;
+	    }
+	  })(undefined, function () {});
+	  (function (global, factory) {
+	    if (true) {
+	      !(__WEBPACK_AMD_DEFINE_ARRAY__ = [exports], __WEBPACK_AMD_DEFINE_FACTORY__ = (factory), __WEBPACK_AMD_DEFINE_RESULT__ = (typeof __WEBPACK_AMD_DEFINE_FACTORY__ === 'function' ? (__WEBPACK_AMD_DEFINE_FACTORY__.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__)) : __WEBPACK_AMD_DEFINE_FACTORY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+	    } else if (typeof exports !== "undefined") {
+	      factory(exports);
+	    } else {
+	      var mod = {
+	        exports: {}
+	      };
+	      factory(mod.exports);
+	      global.index = mod.exports;
+	    }
+	  })(undefined, function (exports) {
+	    'use strict';
+
+	    Object.defineProperty(exports, "__esModule", {
+	      value: true
 	    });
-	  },
+	    exports.find = find;
+	    exports.load = load;
+	    exports.all = all;
+	    exports.exist = exist;
+	    exports.getUrl = getUrl;
+	    exports.parse = parse;
 
-	  template: '\n    <div>\n      {{repo.name}}\n      Stars: {{repo.stargazers_count}}\n      Forks: {{repo.forks_count}}\n    </div>\n  '
+	    function _toConsumableArray(arr) {
+	      if (Array.isArray(arr)) {
+	        for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) {
+	          arr2[i] = arr[i];
+	        }
+
+	        return arr2;
+	      } else {
+	        return Array.from(arr);
+	      }
+	    }
+
+	    var enpoint = 'https://api.github.com/emojis';
+	    var delimiterRegex = /(\:[\w\-\+]+\:)/g;
+	    var emojis = null;
+
+	    var fetch = window.fetch || function (endpoint) {
+	      return new Promise(function (resolve, reject) {
+	        var xhr = new XMLHttpRequest();
+
+	        xhr.onreadystatechange = function () {
+	          if (xhr.readyState === XMLHttpRequest.DONE) {
+	            if (xhr.status !== 200) {
+	              return reject(xhr.responseText);
+	            }
+
+	            return resolve({ json: function json() {
+	                return JSON.parse(xhr.responseText);
+	              } });
+	          }
+	        };
+
+	        xhr.open('GET', endpoint, true);
+	        xhr.send();
+	      });
+	    };
+
+	    /**
+	     * Return array with matched emojis in text.
+	     *
+	     * @example
+	     * import { load as loadEmojis, find as findEmojis } from 'gh-emoji';
+	     *
+	     * const text = 'Do you believe in :alien:...? :scream:';
+	     *
+	     * loadEmojis().then((emojis) => {
+	     *   console.log(findEmojis(text)); // [':alien:', ':scream:']
+	     * });
+	     *
+	     * @param {String} text Text to search for emojis.
+	     *
+	     * @returns {Array<String>} Array with matched emojis.
+	     */
+	    function find(text) {
+	      return text.match(delimiterRegex) || [];
+	    }
+
+	    /**
+	     * Fetch the emoji data from Github's api.
+	     *
+	     * @example
+	     * import { load as loadEmojis } from 'gh-emoji';
+	     *
+	     * loadEmojis().then((emojis) => {
+	     *   console.log(emojis['+1']); // 👍
+	     * });
+	     *
+	     * @returns {Promise<Object>} Promise which passes Object with emoji names
+	     * as keys and generated image tags as values to callback.
+	     */
+	    function load() {
+	      return new Promise(function (resolve) {
+	        if (emojis) return resolve(emojis);
+
+	        return fetch(enpoint).then(function (r) {
+	          return r.json();
+	        }).then(function (response) {
+	          emojis = response;
+	          resolve(emojis);
+	        });
+	      });
+	    }
+
+	    /**
+	     * Return all fetched emojis.
+	     *
+	     * @example
+	     * import { load as loadEmojis, all as allEmojis } from 'gh-emoji';
+	     *
+	     * loadEmojis().then(() => {
+	     *   console.log(allEmojis()); // {emojiName: emojiImageTag}
+	     * });
+	     *
+	     * @returns {Object} Object with emoji names as keys and generated image tags
+	     * as values.
+	     */
+	    function all() {
+	      return emojis;
+	    }
+
+	    /**
+	     * Check if requested emoji exists.
+	     *
+	     * @example
+	     * import { load as loadEmojis, exist as emojiExists } from 'gh-emoji';
+	     *
+	     * loadEmojis().then(() => {
+	     *   console.log(emojiExists('foo')); // false
+	     *   console.log(emojiExists('smile')); // true
+	     * });
+	     *
+	     * @param {String} emojiId Name of emoji.
+	     *
+	     * @returns {Boolean}
+	     */
+	    function exist(emojiId) {
+	      var emojiMap = all();
+
+	      if (emojiMap == null) {
+	        return false;
+	      }
+
+	      return !!emojiMap[emojiId];
+	    }
+
+	    /**
+	     * Return github's image url of emoji.
+	     *
+	     * @example
+	     * import { load as loadEmojis, getUrl } from 'gh-emoji';
+	     *
+	     * loadEmojis().then(() => {
+	     *   console.log(getUrl('apple')); // 'https://assets-cdn.github.com/images/icons/emoji/unicode/1f34e.png?v6'
+	     * });
+	     *
+	     * @param {String} emojiId Name of emoji.
+	     *
+	     * @returns {String} Image url of given emoji.
+	     */
+	    function getUrl(emojiId) {
+	      var emojiMap = all();
+
+	      if (emojiMap == null) {
+	        return null;
+	      }
+
+	      return emojiMap[emojiId];
+	    }
+
+	    /**
+	     * Parse text and replace emoji tags with actual emoji symbols.
+	     *
+	     * @example
+	     * import { load as loadEmojis, parse } from 'gh-emoji';
+	     *
+	     * const text = 'Do you believe in :alien:...? :scream:';
+	     *
+	     * loadEmojis().then(() => {
+	     *   console.log(parse(text)) // 'Do you believe in 👽...? 😱';
+	     * });
+	     *
+	     * @param {String} text Text to parse.
+	     * @param {Object} options Options with additional data for parser.
+	     * @param {String} options.classNames String with custom class names
+	     * added to each emoji, separated with whitespace.
+	     *
+	     * @returns {String} Parsed text with emoji image tags in it.
+	     */
+	    function parse(text) {
+	      var options = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+
+	      var output = '';
+	      var customClassNames = options.classNames ? options.classNames.trim().split(/\s+/) : '';
+
+	      output += text.replace(delimiterRegex, function (match) {
+	        var name = match.replace(/:/g, '');
+	        var classNames = ['gh-emoji', 'gh-emoji-' + name];
+
+	        if (!exist(name)) {
+	          return match;
+	        }
+
+	        if (customClassNames) {
+	          classNames.push.apply(classNames, _toConsumableArray(customClassNames));
+	        }
+
+	        var imageSrc = getUrl(name);
+	        var imageClass = classNames.join(' ');
+	        var imageAlt = name;
+
+	        return '<img src="' + imageSrc + '" class="' + imageClass + '" alt="' + imageAlt + '" />';
+	      });
+
+	      return output;
+	    }
+	  });
 	});
 
 /***/ }
